15ad066bddff53b438a553964295a29c339095fe
Martin Stiborský <martin.stiborsky@gmail.com>
2022-07-13 09:11:29 +0200

    zsh key bindings updated

--- zsh/.zshrc.d/keybindings.zsh
+++ zsh/.zshrc.d/keybindings.zsh
@@ -45,3 +45,10 @@ if (( ${+terminfo[smkx]} && ${+terminfo[rmkx]} )); then
 	add-zle-hook-widget -Uz zle-line-init zle_application_mode_start
 	add-zle-hook-widget -Uz zle-line-finish zle_application_mode_stop
 fi
+
+# Enable Ctrl-x-e to edit command line
+autoload -U edit-command-line
+# Emacs style
+zle -N edit-command-line
+bindkey '^xe' edit-command-line
+bindkey '^x^e' edit-command-line
